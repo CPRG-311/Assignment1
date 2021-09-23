4aa4bfe995ca03d8d5ef3a444684492f12a3e1de
ashleyDrinkill <91289027+ashleyDrinkill@users.noreply.github.com>
2021-09-23 15:04:44 -0600

    Create Shape.java

---
+++ Assignment_1/Assignment_1/src/assignmentManagementApplication/Shape.java
@@ -0,0 +1,19 @@
+package assignmentManagementApplication;
+
+import java.util.Comparator;
+
+public class Shape implements Comparable, Comparator{
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
