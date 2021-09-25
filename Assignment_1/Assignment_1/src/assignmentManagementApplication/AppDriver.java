b9990021d3485a504093df49a5851b4e9d0f966e
Tomnguyen2604 <63090801+Tomnguyen2604@users.noreply.github.com>
2021-09-24 18:56:51 -0600

    update on the driver

--- Assignment_1/Assignment_1/src/assignmentManagementApplication/AppDriver.java
+++ Assignment_1/Assignment_1/src/assignmentManagementApplication/AppDriver.java
@@ -32,11 +32,48 @@ public class AppDriver {
 			// all of the specific shapes
 			// you have to read in the shape type and create that specific shape,
 			// eg. if it reads cone: Shape shape = (Shape)new Cone(num1, num2);
-			Shape shape = new Shape(shapeName,number1,number2);
+			Shape shape = new Shape(shapeName);
 			shape.setShapeName(shapeName);
-			shape.setNumber1(number1);
-			shape.setNumber2(number2);
-			newArry[counter] = shape;
+			
+			Shape cone = (Shape)new Shape(number1, number2);
+			cone.setNumber1(number1);
+			cone.setNumber2(number2);
+			newArry[counter] = cone;
+			
+			Shape cylinder = (Shape)new Shape(number1, number2);
+			cylinder.setNumber1(number1);
+			cylinder.setNumber2(number2);
+			newArry[counter] = cylinder;
+			
+			Shape octagonalPrism = (Shape)new Shape(number1, number2);
+			octagonalPrism.setNumber1(number1);
+			octagonalPrism.setNumber2(number2);
+			newArry[counter] = octagonalPrism;
+			
+			Shape pentagonalPrism = (Shape)new Shape(number1, number2);
+			pentagonalPrism.setNumber1(number1);
+			pentagonalPrism.setNumber2(number2);
+			newArry[counter] = pentagonalPrism;
+			
+			Shape prism = (Shape)new Shape(number1, number2);
+			prism.setNumber1(number1);
+			prism.setNumber2(number2);
+			newArry[counter] = prism;
+			
+			Shape pyramid = (Shape)new Shape(number1, number2);
+			pyramid.setNumber1(number1);
+			pyramid.setNumber2(number2);
+			newArry[counter] = pyramid;
+			
+			Shape squarePrism = (Shape)new Shape(number1, number2);
+			squarePrism.setNumber1(number1);
+			squarePrism.setNumber2(number2);
+			newArry[counter] = squarePrism;
+			
+			Shape triangularPrism = (Shape)new Shape(number1, number2);
+			triangularPrism.setNumber1(number1);
+			triangularPrism.setNumber2(number2);
+			newArry[counter] = triangularPrism;
 			
 			
 			counter++;
@@ -44,10 +81,6 @@ public class AppDriver {
 				
 			
 			}
-			
-			
-			
-			
 			file.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- Assignment_1/Assignment_1/src/assignmentManagementApplication/Shape.java
+++ Assignment_1/Assignment_1/src/assignmentManagementApplication/Shape.java
@@ -5,16 +5,24 @@ import java.util.Comparator;
 
 public class Shape implements Comparable, Comparator{
 	
-	
+	public Shape(Double number1, Double number2) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public int compare(Object o1, Object o2) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public int compareTo(Object o) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
+
+
+
+
+
 }
