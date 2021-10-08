b6bacead6288c2423d699c7c603698367886845f
ashleyDrinkill <91289027+ashleyDrinkill@users.noreply.github.com>
2021-10-08 11:35:17 -0600

    Documenting

--- Assignment-1/src/assignmentManagementApplication/Pyramid.java
+++ Assignment-1/src/assignmentManagementApplication/Pyramid.java
@@ -1,5 +1,13 @@
 package assignmentManagementApplication;
 
+/**
+ * @author Ashley Drinkill, Gia Hoa (Tom) Hyugen, Kolby Robertson, Ethan Wright
+ * This class is an extension of a Shape, and creates a Pyramid Shape object
+ * It requires a: 
+ * 	height
+ * 	edge length
+ *
+ */
 public class Pyramid extends Shape {
 	double sideLength;
 	
@@ -17,11 +25,21 @@ public class Pyramid extends Shape {
 		this.sideLength = sideLength;
 	}
 
+	/**
+	 * implements the area formula for Pyramids, an overridden method of the
+	 * Shape abstract class
+	 * formula: side**2
+	 */
 	@Override
 	public double calculateBaseArea() {
 		return Math.pow(sideLength,2);
 	}
 
+	/**
+	 * implements the volume formula for Pyramids, an overridden method of the
+	 * Shape abstract class
+	 * formula: side**2 * height * (1/3)
+	 */
 	@Override
 	public double calculateVolume() {
 		double baseArea = this.calculateBaseArea();
