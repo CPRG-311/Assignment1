63684e109c982031a5db909d1a2083c399f3826f
826847 <826847@SAIT224196IT.ACDM.DS.SAIT.CA>
2021-10-08 11:43:38 -0600

    added the documentation for the prim shapes

--- Assignment-1/src/assignmentManagementApplication/OctagonalPrism.java
+++ Assignment-1/src/assignmentManagementApplication/OctagonalPrism.java
@@ -1,6 +1,16 @@
 package assignmentManagementApplication;
 
 public class OctagonalPrism extends Prism {
+	/**
+	 * @author Ethan Wright,Gia Hoa (Tom) Nguyen, Ashley Drinkill, Kolby Robertson
+	 * 
+	 * This method takes in the height and side length from the App Driver
+	 * it uses super to get the value from the parent which is shape,
+	 * then sets the values it gets from shape to it's corresponding double value
+	 * 
+	 * @param height - takes in the height from the AppDriver
+	 * @param side - takes in the side length from the AppDriver
+	 */
 	
 	public OctagonalPrism(double height, double side) {
 		super();
@@ -8,11 +18,26 @@ public class OctagonalPrism extends Prism {
 		super.sideLength = side;
 	}
 
+	/**
+	 * @author Ethan Wright,Gia Hoa (Tom) Nguyen, Ashley Drinkill, Kolby Robertson
+	 * 
+	 *This method calculates the base area for the OctagonalPrism
+	 *
+	 *@return a - returns the value it's calculated for the base area
+	 */
 	@Override
 	public double calculateBaseArea() {
 		double a = 2 * (1 + Math.sqrt(2)) * Math.pow(sideLength, 2);
 		return a;
 	}
+	
+	/**
+	 * @author Ethan Wright,Gia Hoa (Tom) Nguyen, Ashley Drinkill, Kolby Robertson
+	 * 
+	 * This method calculates the volume for the octagonalprism
+	 * 
+	 * @return baseArea - returns the calculated baseArea
+	 */
 
 	@Override
 	public double calculateVolume() {
--- Assignment-1/src/assignmentManagementApplication/Prism.java
+++ Assignment-1/src/assignmentManagementApplication/Prism.java
@@ -3,9 +3,25 @@ package assignmentManagementApplication;
 public abstract class Prism extends Shape {
 	double sideLength;
 	
+	/**
+	 * @author Ethan Wright,Gia Hoa (Tom) Nguyen, Ashley Drinkill, Kolby Robertson
+	 * 
+	 * This method gets the side length and returns it for the prism shapes that are based off the prism.
+	 * 
+	 * 
+	 * @return sideLength - returns the side length for the prisms that need it.
+	 */
 	public double getSideLength() {
 		return sideLength;
 	}
+	
+	/**
+	 * @author Ethan Wright,Gia Hoa (Tom) Nguyen, Ashley Drinkill, Kolby Robertson
+	 * 
+	 * This method sets the side length for the prims.
+	 * 
+	 * @param sideLength - this parameter contains the value for the side length.
+	 */
 	public void setSideLength(double sideLength) {
 		this.sideLength = sideLength;
 	}
